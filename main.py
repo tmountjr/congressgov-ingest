34d9bce389dee84c903484733f7b68d7c7422bdb
Tom Mount <tmountjr@gmail.com>
2025-04-11 16:30:04 -0400

    add vote metadata table import

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ from database.bills import (
   create_table as create_bills_table,
   populate as populate_bills_table
 )
+from database.vote_meta import (
+  drop_table as drop_vote_meta_table,
+  create_table as create_vote_meta_table,
+  populate as populate_vote_meta_table
+)
 
 if __name__ == "__main__":
     print("Importing legislators...")
@@ -23,3 +28,8 @@ if __name__ == "__main__":
     drop_bills_table()
     create_bills_table()
     populate_bills_table()
+
+    print("Importing vote metadata...")
+    drop_vote_meta_table()
+    create_vote_meta_table()
+    populate_vote_meta_table()

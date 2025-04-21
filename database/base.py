5242e102f0c147337df963470cac1db47fc0ec5c
Tom Mount <tmountjr@gmail.com>
2025-04-21 15:21:28 -0400

    can't run multiple statements in view definition

--- database/base.py
+++ database/base.py
@@ -13,8 +13,7 @@ with Session(engine) as session:
     # Create a view that shows only the last vote on each vote ID.
     session.execute(
         text(
-            """DROP VIEW IF EXISTS "main"."latest_vote_ids";
-CREATE VIEW latest_vote_ids as
+            """CREATE VIEW IF NOT EXISTS latest_vote_ids as
 	with temp_vm as (
 		select
 			*,
@@ -41,8 +40,7 @@ CREATE VIEW latest_vote_ids as
     # Create a view that adds sponsor name and party to the vote_meta table.
     session.execute(
         text(
-            """DROP VIEW IF EXISTS "main"."enriched_vote_meta";
-CREATE VIEW enriched_vote_meta as
+            """CREATE VIEW IF NOT EXISTS enriched_vote_meta as
   select vote_meta.*, legislators.name as sponsor_name, legislators.party as sponsor_party
   from vote_meta
   left join bills on vote_meta.bill_id = bills.bill_id
